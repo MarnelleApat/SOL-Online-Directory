0fc5a573dabe9c44491f6da989a971c0a9f1d1f9
Keneth John Baguinat <keneth.baguinat@bible.org.sg>
2022-10-12 15:00:12 +0800

    added changes to create partners

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -6,15 +6,18 @@ use Illuminate\Http\Request;
 use App\Models\Department;
 use Inertia\Inertia;
 use Illuminate\Support\Str;
+use Redirect;
+use App\Http\Requests\DepartmentRequest;
 
 class DepartmentController extends Controller
 {
     public function index()
     {
-        $partners = Department::with('users')->orderBy('id','desc')->paginate();
+        $partners = Department::with('users')->orderBy('id','desc')->paginate(7);
 
         return Inertia::render('Partners/index', [
-            'partners' => $partners
+            'partners' => $partners,
+            'isOpen' => false
         ]);
     }
 
@@ -23,22 +26,20 @@ class DepartmentController extends Controller
         return Inertia::render('Partners/create');
     }
 
-    public function store(Request $request)
+    public function store(DepartmentRequest $request)
     {
 
         $partners = Department::create([
             'name' => $request->name,
-            'slug' => Str::slug($request->name),
+            'slug' => $this->createSlug($request->name),
             'description' => $request->description,
             'websiteUrl' => $request->websiteUrl,
             'status' => 1,
         ]);
 
-        $partners = Department::with('users')->paginate();
+        if($partners)
+            return Redirect::route('partners.index');
 
-        return Inertia::render('Partners/index', [
-            'partners' => $partners
-        ]);
     }
 
     public function delete(Request $request)
@@ -59,5 +60,32 @@ class DepartmentController extends Controller
         return Inertia::render('Partners/view', [
             'partners' => $partners
         ]); 
+    }
+
+    private function createSlug($title, $id = 0)
+    {
+        $slug = Str::slug($title);
+        $allSlugs = $this->getRelatedSlugs($slug, $id);
+        if (! $allSlugs->contains('slug', $slug)){
+            return $slug;
+        }
+
+        $i = 1;
+        $is_contain = true;
+        do {
+            $newSlug = $slug . '-' . $i;
+            if (!$allSlugs->contains('slug', $newSlug)) {
+                $is_contain = false;
+                return $newSlug;
+            }
+            $i++;
+        } while ($is_contain);
+    }
+    
+    private function getRelatedSlugs($slug, $id = 0)
+    {
+        return Department::select('slug')->where('slug', 'like', $slug.'%')
+        ->where('id', '<>', $id)
+        ->get();
     }
 }
